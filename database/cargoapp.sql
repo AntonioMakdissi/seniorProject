c8aaba70fb50329506e845dc2341894f530d5991
Antonio Al Makdissi <antoniomakdissi1@gmail.com>
2023-04-12 12:28:16 +0300

    updated word doc

--- database/cargoapp.sql
+++ database/cargoapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2023 at 05:14 PM
+-- Generation Time: Apr 12, 2023 at 11:26 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -36,8 +36,10 @@ CREATE TABLE `branches` (
 --
 
 INSERT INTO `branches` (`branch`) VALUES
+('Akkar'),
 ('Al Koura'),
 ('Batroun'),
+('Beirut'),
 ('delivered'),
 ('still at client');
 
@@ -62,6 +64,14 @@ CREATE TABLE `clients` (
   `rating` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`c_id`, `u_id`, `pay_id`, `c_name`, `c_phone`, `c_address`, `c_longitude`, `c_latitude`, `c_district`, `c_timestamp`, `guest`, `rating`) VALUES
+(6, 8, NULL, 'test01', 'test01', 'test01', NULL, NULL, 'Batroun', '2023-03-31 12:18:06', 0, NULL),
+(7, 9, NULL, 'zaher', 'zaher', 'zaher', NULL, NULL, 'Batroun', '2023-04-03 06:04:07', 0, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +86,27 @@ CREATE TABLE `deliveries` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `deliveries`
+--
+
+INSERT INTO `deliveries` (`d_id`, `w_id`, `o_id`, `current_location`, `timestamp`) VALUES
+(1, NULL, 1, 'still at client', '2023-04-10 08:07:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `m_id` int(11) NOT NULL,
+  `send_id` int(11) NOT NULL,
+  `receive_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- --------------------------------------------------------
 
 --
@@ -86,8 +117,17 @@ CREATE TABLE `orders` (
   `o_id` int(11) NOT NULL,
   `p_id` int(11) DEFAULT NULL,
   `c_id` int(11) DEFAULT NULL,
-  `status` enum('successful','pending','failed','') NOT NULL DEFAULT 'pending'
+  `status` enum('successful','pending','failed','') NOT NULL DEFAULT 'pending',
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`o_id`, `p_id`, `c_id`, `status`, `date`) VALUES
+(1, 1, 6, 'pending', '2023-04-10 07:17:10'),
+(2, 2, 7, 'successful', '2023-04-10 07:59:42');
 
 -- --------------------------------------------------------
 
@@ -101,15 +141,25 @@ CREATE TABLE `packages` (
   `height` float NOT NULL,
   `weight` float NOT NULL,
   `message` text DEFAULT NULL,
+  `to_name` varchar(255) DEFAULT NULL,
   `to_address` varchar(255) NOT NULL,
-  `p_longitude` decimal(11,8) NOT NULL,
-  `p_latitude` decimal(10,8) NOT NULL,
+  `p_longitude` decimal(11,8) DEFAULT NULL,
+  `p_latitude` decimal(10,8) DEFAULT NULL,
   `to_district` varchar(255) NOT NULL,
   `fragile` tinyint(1) NOT NULL DEFAULT 0,
   `o_price` float NOT NULL DEFAULT 0,
   `cost` float NOT NULL,
-  `f_price` float NOT NULL
+  `f_price` float NOT NULL,
+  `pay_at_delivery` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`p_id`, `width`, `height`, `weight`, `message`, `to_name`, `to_address`, `p_longitude`, `p_latitude`, `to_district`, `fragile`, `o_price`, `cost`, `f_price`, `pay_at_delivery`) VALUES
+(1, 30, 30, 5, 'to my love', NULL, 'kfaraaka', NULL, NULL, 'Al Koura', 0, 0, 5, 5, 1),
+(2, 50, 100, 20, NULL, NULL, 'kfarsaroun', NULL, NULL, 'Al Koura', 0, 0, 7, 7, 1);
 
 -- --------------------------------------------------------
 
@@ -145,7 +195,20 @@ INSERT INTO `privileges` (`privilege`) VALUES
 ('BranchManager'),
 ('CEO'),
 ('client'),
+('IT'),
 ('worker');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `o_id` int(11) DEFAULT NULL,
+  `w_id` int(11) DEFAULT NULL,
+  `deadline` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -165,7 +228,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`u_id`, `email`, `password`, `type`) VALUES
-(1, 'test@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', 'client');
+(1, 'CEO@hotmail.com', '858904c3e266f5640bfa88f16d2ed50a', 'CEO'),
+(2, 'IT@hotmail.com', 'cd32106bcb6de321930cf34574ea388c', 'IT'),
+(3, 'worker1@hotmail.com', 'ebad64149cc767ba26ef069819279fd5', 'worker'),
+(8, 'test01@wow', '0e698a8ffc1a0af622c7b4db3cb750cc', 'client'),
+(9, 'zaher@hotmail.com', '2a4e7d2de385a10968b001de2bc66adf', 'client');
 
 -- --------------------------------------------------------
 
@@ -183,6 +250,14 @@ CREATE TABLE `workers` (
   `salary` double DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `workers`
+--
+
+INSERT INTO `workers` (`w_id`, `u_id`, `name`, `phone`, `branch`, `dateOfBirth`, `salary`, `timestamp`) VALUES
+(1, 1, 'CEO', '71787766', 'Beirut', '1993-04-01', NULL, '2023-04-10 09:13:59'),
+(2, 2, 'IT', '71787767', 'Beirut', '1993-04-01', 1000, '2023-04-10 09:15:38');
 
 --
 -- Indexes for dumped tables
@@ -213,6 +288,14 @@ ALTER TABLE `deliveries`
   ADD KEY `current_location` (`current_location`);
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`m_id`),
+  ADD KEY `send_id` (`send_id`),
+  ADD KEY `receive_id` (`receive_id`);
+
+--
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
@@ -224,7 +307,8 @@ ALTER TABLE `orders`
 -- Indexes for table `packages`
 --
 ALTER TABLE `packages`
-  ADD PRIMARY KEY (`p_id`);
+  ADD PRIMARY KEY (`p_id`),
+  ADD KEY `to_district` (`to_district`);
 
 --
 -- Indexes for table `payment`
@@ -237,6 +321,13 @@ ALTER TABLE `payment`
 --
 ALTER TABLE `privileges`
   ADD PRIMARY KEY (`privilege`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD KEY `o_id` (`o_id`),
+  ADD KEY `w_id` (`w_id`);
 
 --
 -- Indexes for table `users`
@@ -261,25 +352,25 @@ ALTER TABLE `workers`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `deliveries`
 --
 ALTER TABLE `deliveries`
-  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `packages`
 --
 ALTER TABLE `packages`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `payment`
@@ -291,13 +382,13 @@ ALTER TABLE `payment`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `workers`
 --
 ALTER TABLE `workers`
-  MODIFY `w_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `w_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -320,11 +411,31 @@ ALTER TABLE `deliveries`
   ADD CONSTRAINT `deliveries_ibfk_3` FOREIGN KEY (`current_location`) REFERENCES `branches` (`branch`) ON UPDATE CASCADE;
 
 --
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`send_id`) REFERENCES `workers` (`w_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receive_id`) REFERENCES `workers` (`w_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `clients` (`c_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `packages` (`p_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `packages`
+--
+ALTER TABLE `packages`
+  ADD CONSTRAINT `packages_ibfk_1` FOREIGN KEY (`to_district`) REFERENCES `branches` (`branch`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`o_id`) REFERENCES `orders` (`o_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`w_id`) REFERENCES `workers` (`w_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
