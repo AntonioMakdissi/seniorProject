368b538640cc900b2d5fca6ac3e5a944ca21f84b
FawzyGhazawy <127242609+FawzyGhazawy@users.noreply.github.com>
2023-04-24 02:39:24 +0300

    Add files via upload

--- php/track.php
+++ php/track.php
@@ -9,27 +9,144 @@ if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
 <head>
   <title>Track</title>
   <meta charset="UTF-8" />
+  <link href="../assets/css/main.css" rel="stylesheet">
   <style>
-    table {
-      border-collapse: collapse;
+    .container {
+      margin-top: 50px;
+    }
+
+    h1 {
+      text-align: center;
+      margin-bottom: 50px;
+    }
+
+    .table {
       width: 100%;
+      max-width: 1200px;
+      margin: 0 auto;
+      border-collapse: separate;
+      border-spacing: 0 10px;
     }
 
-    tr {
-      border-bottom: 1px solid #ddd;
+    .table th,
+    .table td {
+      padding: 12px;
+      vertical-align: middle;
+      text-align: center;
     }
 
-    th {
-      background-color: rgb(1, 1, 24);
-      color: white;
+    .table th {
+      background-color: #f5f5f5;
+      font-weight: bold;
+      text-transform: uppercase;
     }
+
+    .table tbody tr {
+      background-color: #fff;
+      border-radius: 4px;
+      box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+    }
+
+    .table-striped tbody tr:nth-of-type(odd) {
+      background-color: #f5f5f5;
+    }
+
+    body {
+      background: url('https://th.bing.com/th/id/R.ea4192babfe60404ef0dafb21484f282?rik=doxyDdbyqpjM2Q&pid=ImgRaw&r=0') center center fixed no-repeat;
+      background-size: cover;
+      position: relative;
+    }
+
+    body::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background-color: rgba(255, 255, 255, 0.01);
+      /* Change opacity as needed */
+    }
+
+    .navbar {
+      position: relative;
+      z-index: 9999;
+    }
+    .navbar ul {
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  justify-content: flex-end;
+}
+ 
+select {
+  cursor: pointer;
+    appearance: none;
+    -webkit-appearance: none;
+    color: #555;
+    font-size: 16px;
+    padding: 10px 30px 10px 10px;
+    border: none;
+    border-radius: 5px;
+    width: 200px;
+  }
+
+  select::-ms-expand {
+    display: none;
+  }
   </style>
 </head>
 
 <body>
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-  <h2>Track your order</h2>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="../client.php" class="active">Home</a></li>
+          <li><a href="../about.html">About</a></li>
+          <li><a href="../services.html">Services</a></li>
+          <li><a href="history.php">History</a></li>
+          <li><a href="track.php">Track</a></li>
+          <li><a href="../contact.html">Contact</a></li>
+          <li><a class="get-a-quote" href="logout.php">Logout</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
 
+<<<<<<< HEAD
+ 
+  
+ 
+  <div class="container" style="margin-top: 10px">
+  <h1 style="color: white">Track your order</h1>
+ 
+  <div style="display: flex; justify-content: center;">
+ 
+  <select style="position: relative; display: inline-block;  margin: 20px;">
+  <option value="" disabled selected>Select your order id</option>
+<?php
+require_once('connection.php');
+$c_id = $_SESSION['c_id'];
+$query = "SELECT o_id FROM orders WHERE c_id='$c_id' ORDER BY o_id";
+$result = mysqli_query($link, $query);
+if (($result) && (mysqli_num_rows($result) > 0)) {
+
+  while ($row = mysqli_fetch_assoc($result)) { ?>
+    <option value=<?php echo $row["o_id"] ?>> <?php echo $row["o_id"] ?></option>
+<?php }
+} ?>
+</select>
+  </div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+=======
   <!-- get orders id -->
   <?php
   require_once('connection.php');
@@ -56,6 +173,7 @@ if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
   <table border="border" id="trackTable">
     <thead>
       <tr>
+>>>>>>> 6fe4dd4154d9999b657359e2e445e485df94994e
         <th>Date and time</th>
         <th>Location</th>
         <th>By</th>
@@ -116,6 +234,28 @@ if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
 </body>
 
 </html>
+<<<<<<< HEAD
+<!-- code to connect select with table 
+const select = document.getElementById('filter-select');
+const table = document.getElementById('table');
+
+select.addEventListener('change', (event) => {
+  const filterValue = event.target.value;
+
+  // Loop through each row of the table body
+  for (let i = 0; i < table.tBodies[0].rows.length; i++) {
+    const row = table.tBodies[0].rows[i];
+
+    // If the filter value is "all" or matches the data in the first cell of the row, show the row, otherwise hide it
+    if (filterValue === 'all' || row.cells[0].textContent === filterValue) {
+      row.style.display = '';
+    } else {
+      row.style.display = 'none';
+    }
+  }
+});
+ -->
+=======
 
 <!-- script -->
 <script>
@@ -133,3 +273,4 @@ if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
     }
   }
 </script>
+>>>>>>> 6fe4dd4154d9999b657359e2e445e485df94994e
--- php/viewWorker.php
+++ php/viewWorker.php
@@ -10,38 +10,136 @@ if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
 <head>
     <title>Workers</title>
     <meta charset="UTF-8" />
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
+ <!-- Template Main CSS File -->
+ <link href="../assets/css/main.css" rel="stylesheet">
+  <style>
+    .container {
+      margin-top: 50px;
+    }
 
-        tr {
-            border-bottom: 1px solid #ddd;
-        }
+    h1 {
+      text-align: center;
+      margin-bottom: 50px;
+    }
 
-        th {
-            background-color: rgb(1, 1, 24);
-            color: white;
-        }
-    </style>
+    .table {
+      width: 100%;
+      max-width: 1200px;
+      margin: 0 auto;
+      border-collapse: separate;
+      border-spacing: 0 10px;
+    }
+
+    .table th,
+    .table td {
+      padding: 12px;
+      vertical-align: middle;
+      text-align: center;
+    }
+
+    .table th {
+      background-color: #f5f5f5;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+
+    .table tbody tr {
+      background-color: #fff;
+      border-radius: 4px;
+      box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+    }
+
+    .table-striped tbody tr:nth-of-type(odd) {
+      background-color: #f5f5f5;
+    }
+
+    body {
+      background: url('https://th.bing.com/th/id/R.ea4192babfe60404ef0dafb21484f282?rik=doxyDdbyqpjM2Q&pid=ImgRaw&r=0') center center fixed no-repeat;
+      background-size: cover;
+      position: relative;
+    }
+
+    body::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background-color: rgba(255, 255, 255, 0.01);
+      /* Change opacity as needed */
+    }
+
+    .navbar {
+      position: relative;
+      z-index: 9999;
+    }
+    .navbar ul {
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  justify-content: flex-end;
+}
+
+.custom-button {
+    background-color: #dc3545;
+    border-color: #dc3545;
+    color: #fff;
+    padding: 0.375rem 0.75rem;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+position: relative;
+      z-index: 9999;
+}
+
+.custom-button:hover {
+    background-color: #c82333;
+    border-color: #bd2130;
+    color: #fff;
+}
+
+.custom-button:focus {
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(220, 53, 69, 0.5);
+}
+
+  </style>
 </head>
 
 <body>
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-    <h2>Workers</h2>
-
-
-    <table border="border">
-        <thead>
-            <tr>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="../CEO.php" class="active">Home</a></li>
+          <li><a href="viewWorker.php">Workers</a></li>
+          <li><a href="../CEO.php#branches">Branches</a></li>
+          <li><a href="profit.php">Statistics</a></li>
+          <li><a href="../CEO.php#IT_message">Contact</a></li>
+          <li><a  class="get-a-quote" href="logout.php">Logout</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
+  <div class="container" style="margin-top: 10px">
+    <h1 style="color:white">Workers</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
                 <th>Worker ID </th>
                 <th>name</th>
                 <th>dateOfBirth</th>
                 <th>branch</th>
                 <th>salary</th>
                 <th>date joined</th>
-
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
@@ -61,8 +159,8 @@ if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
 <td>" . $row["dateOfBirth"] . "</td>
 <td>" . $row["branch"] . "</td>
 <td>" . $row["salary"] . "$</td>
-<td>" . $row["timestamp"] . "</td>
-<td> <a class='btn btn-primary btn-sm' name='n' value=$rmid href=http://localhost/web_project/removeworker.php?rmid=" . $rmid . ">Remove</a> </td>
+<td>" . $row["timestamp"] . "</td> 
+<td> <a  name='n' value=$rmid href=http://localhost/seniorProject/php/fireworker.php?rmid=" . $rmid . "> <button type=\"submit\" class=\"custom-button\">Fire</button></a> </td>
 
                           
 </tr>";
