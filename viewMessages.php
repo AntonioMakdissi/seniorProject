acd06372171fb99b7d85e1935d9f18a3567976e0
FawzyGhazawy <ghazawyfawzy@gmail.com>
2023-05-27 09:24:47 +0300

    viewMessages done

--- viewMessages.php
+++ viewMessages.php
@@ -34,6 +34,22 @@ require_once("php/connection.php");
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
+    <style>
+    /* .container {
+        display: flex; 
+    }
+    .messages-container,
+    .form-container {
+        flex: 1;
+        padding: 1em; 
+    } */
+    
+  
+
+</style>
+
+
+
 </head>
 
 <body class="bg-gray-100">
@@ -136,30 +152,36 @@ require_once("php/connection.php");
         </div>
     </header>
     <section id="hero" class="hero d-flex align-items-center" style="padding-top: 20px; height: 100%; ">
-        <div class="container">
+    <div class="container mx-auto px-4 py-12 flex">
 
             <div class="container mx-auto px-4 py-12">
-                <button class="btn-msg-view"><i class="fas fa-envelope"></i></button>
-
+            <div class="form-container" id="form_container">
+  <div class="form-header">
+    <div>
+      <button class="btn-msg-view" id="showMsg"><i class="fas fa-envelope"></i></button>
+      <h1 class="text-4xl font-bold" style="margin-left: 10px;">Send Messages</h1>
+    </div>
+  </div>
+  <form id="messageForm" method="POST" action="php/message.php">
+    <label for="author">Email:</label>
+    <input type="email" id="author" name="email" class="form-input"><br>
+    <label for="content">Message:</label>
+    <textarea id="content" name="message" class="form-input"></textarea><br>
+    <button type="submit" class="btn-msg" class="btn_submit">Send</button>
+  </form>
+</div>
                 <div class="messages-container" id="message_div">
-                    <!-- <div class="message">
-                        <h3 class="message-author">CEO</h3>
-                        <p class="message-content">This is the first message from the CEO.</p>
-                        <p class="message-timestamp">2023-05-02 10:00</p>
-                        <button class="btn-msg">Delete</button>
-                    </div>
-                    <div class="message">
-                        <h3 class="message-author">CEO</h3>
-                        <p class="message-content">This is the second message from the CEO.</p>
-                        <p class="message-timestamp">2023-05-02 10:05</p>
-                        <button class="btn-msg">Delete</button>
-                    </div> -->
-
+                
                     <?php
                     include('php/message.php');
                     $all = viewMessages($link, $_SESSION['u_id']);
                     if ($all == -1) {
-                        echo "no messages";
+                        echo "<div class=\"message\">
+                        <h3 class=\"message-author\">System</h3>
+                        <p class=\"message-content\">Checking messages...</p>
+                        <p class=\"message-timestamp\">Now</p>
+                        <button class=\"btn-danger\" >No messages</button>
+                    </div>";
                     } else {
                         for ($i = 0; $i < count($all[0]); ++$i) {
                             if ($i % 2 == 0) {
@@ -180,17 +202,7 @@ require_once("php/connection.php");
                     }
                     ?>
                 </div>
-                <div class="form-container">
-                    <h1 class="text-4xl font-bold mb-6" style="padding-left:1%;">Send Messages</h1>
-                    <form id="messageForm" method="POST" action="php/message.php">
-                        <label for="author">Email:</label>
-                        <input type="email" id="author" name="email" class="form-input"><br>
-                        <label for="content">Message:</label>
-                        <textarea id="content" name="message" class="form-input"></textarea><br>
-                        <button type="submit" class="btn-msg" class="btn_submit">Send</button>
-                    </form>
-                </div>
-            </div>
+             </div>
         </div>
     </section>
     <script src="assets/vendor/php-email-form/validate.js"></script>
@@ -204,45 +216,26 @@ require_once("php/connection.php");
         }
     </script>
 
-    <!-- <script>
-        document.getElementById('messageForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // to prevent form from submitting and page from reloading
+<!-- js for showMsg button -->
+<script>
+document.getElementById('showMsg').addEventListener('click', function() {
+  var formContainer = document.getElementById('form_container');
+  var messageContainer = document.getElementById('message_div');
 
-    var author = document.getElementById('author').value;
-    var content = document.getElementById('content').value;
-
-    var messageDiv = document.createElement('div');
-    messageDiv.classList.add('message');
-
-    var authorH3 = document.createElement('h3');
-    authorH3.classList.add('message-author');
-    authorH3.textContent = author;
-
-    var contentP = document.createElement('p');
-    contentP.classList.add('message-content');
-    contentP.textContent = content;
-
-    var timestampP = document.createElement('p');
-    timestampP.classList.add('message-timestamp');
-    var timestamp = new Date();
-    timestampP.textContent = timestamp.getFullYear() + '-' + (timestamp.getMonth() + 1) + '-' + timestamp.getDate() + ' ' + timestamp.getHours() + ':' + timestamp.getMinutes();
-
-    var deleteButton = document.createElement('button');
-    deleteButton.classList.add('btn-msg');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function() {
-        messageDiv.remove();
-    });
-
-    messageDiv.appendChild(authorH3);
-    messageDiv.appendChild(contentP);
-    messageDiv.appendChild(timestampP);
-    messageDiv.appendChild(deleteButton);
-
-    document.getElementById('message_div').appendChild(messageDiv);
+  if (messageContainer.classList.contains('hidden')) {
+    messageContainer.classList.remove('hidden');
+    formContainer.classList.remove('form-container-center');
+    formContainer.classList.add('form-container-left');
+  } else {
+    messageContainer.classList.add('hidden');
+    formContainer.classList.remove('form-container-left');
+    formContainer.classList.add('form-container-center');
+  }
 });
 
-    </script> -->
+
+</script>
+
 </body>
 
 </html>
